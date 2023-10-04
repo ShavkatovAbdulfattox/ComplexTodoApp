b5d98ce89d91a337b248da684353f1db720c538b
Avdulfattox <vwhoami@yahoo.com>
2023-10-04 15:51:07 +0500

    localStorage helper is created

--- src/helpers/localStorage.helper.js
+++ src/helpers/localStorage.helper.js
@@ -1,7 +1,20 @@
-export const setToLocalStorage = () => {};
+export const setToLocalStorage = (name, value) => {
+  try {
+    localStorage.setItem(name, JSON.stringify(value));
+  } catch (error) {
+    alert(error);
+  }
+};
 
-export const getFromLocalStorage = () => {
-  let resul;
-
-  
+export const getFromLocalStorage = (name) => {
+  let result = null;
+  try {
+    const localStorageValue = getFromLocalStorage(name);
+    if (localStorageValue) {
+      result = JSON.parse(localStorageValue);
+    }
+  } catch (error) {
+    alert(error);
+  }
+  return result;
 };
